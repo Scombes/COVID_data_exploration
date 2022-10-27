89dde3ab555dea77b3c50d9397219d53737bb367
Scott Combes <scott.combes@gmail.com>
2022-10-27 08:38:29 -0500

    Update data_exploration.sql

--- data_exploration.sql
+++ data_exploration.sql
@@ -1,11 +1,9 @@
-USE covid;
-
 /*
 Covid 19 Data Exploration 
 
-	Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views
+	Skills used: Joins, CTE's, Variables, Windows Functions, Aggregate Functions, Creating Views
 
-	Used data from Our World In Data and created 3 tables:
+	Using data from Our World In Data, I created a database with 3 tables:
 		- cases_deaths : Contains data relating to covid cases and death around the world
 		- vaccinations:  Contains data relating to vacine rates around the world
 		- demographics: Contains demographic data for each country in other two tables
@@ -18,7 +16,7 @@ Covid 19 Data Exploration
 -- LOOKING AT NUMBERS GLOBABLY
 
 /* 
-	Daily totals for population, cases, deaths and vaccinations globally
+ Daily totals for population, cases, deaths and vaccinations globally
 */
 
 SELECT 
@@ -45,7 +43,7 @@ ORDER BY 1
 -- LOOKING AT GENERAL NUMBERS BY COUNTRY
 
 /*
-	Daily total population, cases, deaths and vaccinations by Country
+ Daily total population, cases, deaths and vaccinations by Country
 */
 
 SELECT
@@ -63,47 +61,54 @@ WHERE cd.continent IS NOT NULL
 
 
 /*
-	Total Cases vs Total Deaths
-    - Shows the liklihood of dying if you contract covid in each country
+Total Cases vs Total Deaths
+- Shows the liklihood of dying if you contract covid in each country
 */
 
-SELECT
-	location,
+SELECT 
+    location,
     record_date,
     total_cases,
     total_deaths,
-    (total_deaths/total_cases)*100 AS death_percentage
-FROM cases_deaths
-WHERE continent IS NOT NULL
-ORDER BY 1,2
+    (total_deaths / total_cases) * 100 AS death_percentage
+FROM
+    cases_deaths
+WHERE
+    continent IS NOT NULL
+ORDER BY 1 , 2
 ;
 
 
 /* 
-	Cases & Deaths Relative to Population: Cumlative by Country
-	- Shows the number of cases and deaths per million people
+Cases & Deaths Relative to Population: Cumlative by Country
+- Shows the number of cases and deaths per million people
 */
 
-SELECT
+SELECT 
     cd.location,
     demo.population,
     MAX(cd.total_cases) AS total_cases,
-    MAX(cd.total_cases)*1000000 / demo.population AS cases_per_million,
+    MAX(cd.total_cases) * 1000000 / demo.population AS cases_per_million,
     MAX(cd.total_deaths) AS total_deaths,
-    MAX(cd.total_deaths)*1000000 /  demo.population AS deaths_per_million
-FROM cases_deaths cd
-LEFT JOIN vaccinations vax
-	ON vax.location = cd.location AND vax.record_date = cd.record_date
-LEFT JOIN demographics demo ON demo.location = cd.location AND demo.record_date = cd.record_date
-WHERE cd.continent IS NOT NULL
-GROUP BY 1, 2
+    MAX(cd.total_deaths) * 1000000 / demo.population AS deaths_per_million
+FROM
+    cases_deaths cd
+        LEFT JOIN
+    vaccinations vax ON vax.location = cd.location
+        AND vax.record_date = cd.record_date
+        LEFT JOIN
+    demographics demo ON demo.location = cd.location
+        AND demo.record_date = cd.record_date
+WHERE
+    cd.continent IS NOT NULL
+GROUP BY 1 , 2
 ORDER BY 1
 ;
 
 
 /*
-	Total Cases vs Population
-	- Shows what percentage of population infected with covid
+Total Cases vs Population
+- Shows what percentage of population infected with covid
 */
 
 SELECT 
@@ -124,9 +129,8 @@ ORDER BY 1 , 2
 
 
 /* 
-	Vacination Rates
-	- The percentage of the population who has received at least one does
-      of the covid vaccine
+Vacination Rates
+- The percentage of the population who has received at least one does of the covid vaccine
 */
 
 SELECT 
@@ -144,6 +148,7 @@ WHERE
     vax.continent IS NOT NULL
 GROUP BY 2 , 3
 ;
+
 
 -- Creating a View to store data for later use
 
@@ -168,9 +173,9 @@ GROUP BY 2 , 3
 -- BREAKING THINGS DOWN BY CONTINENT
 
 /* 
-	Countinents with highest death count per population
-	- Find the MAX total deaths for each country & group by continent, location
-	- Use as a subquery to SUM total death count and group by continent
+Countinents with highest death count per population
+- Find the MAX total deaths for each country & group by continent, location
+- Use as a subquery to SUM total death count and group by continent
 */
 
 SELECT 
@@ -191,10 +196,10 @@ ORDER BY 2 DESC
 ;
 
 /*
-	Cases & Deaths Per Million by Continent
-    -- Breakdown by Month
-    -- First get total cases, deaths and population per month for each country
-    -- Use as a CTE to get cases and deaths per million for each continent
+Cases & Deaths Per Million by Continent
+- Breakdown by Month
+- First get total cases, deaths and population per month for each country
+- Use as a CTE to get cases and deaths per million for each continent
 */
 
 WITH country_monthly_numbers AS
@@ -228,11 +233,11 @@ GROUP BY 1, 2, 3
 -- ADVANCED ANALYSIS
 
 /* 
-	Latest Vacination Rates: Globally
-    - Total doses given worldwide
-    - Change since previous day
-    - Number of people fully vaccinated
-    - Percent of population fully vaccinated
+Latest Vacination Rates: Globally
+- Total doses given worldwide
+- Change since previous day
+- Number of people fully vaccinated
+- Percent of population fully vaccinated
 */
  -- Create a variable that stores the total world population
 SET @world_population = 
@@ -266,11 +271,11 @@ LIMIT 1
 
 
 /* 
-	Quarter Over Quarter Change for Cases and Deaths Globaly
-    - Get total cases and deaths per million. Group by year and quarter
-    - Use as CTE
-    - Use window functions to get previous quarter numbers
-    - Quarter over Quarter change = (Current quarter - previous quarter)/ previous quarter
+Quarter Over Quarter Change for Cases and Deaths Globaly
+- Get total cases and deaths per million. Group by year and quarter
+- Use as CTE
+- Use window functions to get previous quarter numbers
+- Quarter over Quarter change = (Current quarter - previous quarter)/ previous quarter
 */
 
 WITH cte AS 
@@ -297,15 +302,15 @@ FROM cte
 
 
 /*
-	Total Cases and deaths compared to GDP Per Capita  
-    - Sort countries into buckets based on GDP Per Capita
-		- <10k
-        - 10k - 20k
-        - 20k - 30k
-        - 30k - 40k
-        - >40k
-	- Determin cases and deaths per million for each bucket
-    - Count how many countries are in each bucket
+Total Cases and deaths compared to GDP Per Capita  
+- Sort countries into buckets based on GDP Per Capita
+	- <10k
+	- 10k - 20k
+	- 20k - 30k
+	- 30k - 40k 
+    - >40k
+- Determin cases and deaths per million for each bucket
+- Count how many countries are in each bucket
 */
 
 WITH country_cases_gdp AS
